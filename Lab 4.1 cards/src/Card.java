1b286468201a03b71e72f61ebc461483466923aa
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-03-13 09:22:11 -0400

    wha

---
+++ Lab 4.1 cards/src/Card.java
@@ -0,0 +1,24 @@
+
+public class Card {
+	
+	private String rank;
+	private String suit;
+	private int pointValue;
+	
+	Card(String rank, String suit, int pointValue){
+		this.rank = rank;
+		this.suit = suit;
+		this.pointValue = pointValue;
+	}
+	public static void main(String args[]) {
+		Card 
+	}
+	
+	public boolean equals(Card otherCard) {
+		
+	}
+	
+	public String toString() {
+	
+	}
+}
